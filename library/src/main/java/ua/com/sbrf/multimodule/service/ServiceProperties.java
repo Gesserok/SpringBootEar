c82d1814d3c27009cd9489ee7c7ad3464367869b
Gesserok <if2102@outlook.com>
2020-10-28 11:08:38 +0200

    with jar

--- library/src/main/java/ua/com/sbrf/multimodule/service/ServiceProperties.java
+++ library/src/main/java/ua/com/sbrf/multimodule/service/ServiceProperties.java
@@ -1,21 +1,21 @@
-package ua.com.sbrf.multimodule.service;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@ConfigurationProperties("service")
-public class ServiceProperties {
-
-	/**
-	 * A message for the service.
-	 */
-	private String message;
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-}
+//package ua.com.sbrf.multimodule.service;
+//
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.context.annotation.Configuration;
+//
+//@ConfigurationProperties("service")
+//public class ServiceProperties {
+//
+//	/**
+//	 * A message for the service.
+//	 */
+//	private String message;
+//
+//	public String getMessage() {
+//		return message;
+//	}
+//
+//	public void setMessage(String message) {
+//		this.message = message;
+//	}
+//}
