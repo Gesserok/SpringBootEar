06fb865158b7f6e66c95a50173738f289b2daf9c
Gesserok <if2102@outlook.com>
2020-11-03 10:27:48 +0200

    Spring_Data_JPA

---
+++ library/src/main/java/org/example/multimodule/models/Region.java
@@ -0,0 +1,39 @@
+package org.example.multimodule.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Data
+@Entity
+@Table(name = "regions")
+public class Region {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "region_gen")
+    @SequenceGenerator(name = "region_gen", sequenceName = "gen_region_id", allocationSize = 1)
+    @JsonIgnore
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private BigDecimal id;
+
+    @JsonProperty(value = "passports_list")
+    @ElementCollection
+    @CollectionTable(name = "passports", joinColumns = @JoinColumn(name = "region_id"))
+    private List<Passport> passports;
+
+    @JsonProperty("resource_id")
+    @Column(name = "resource_id")
+    private String resourceId;
+
+    @JsonProperty("resource_revision_url")
+    @Column(name = "resource_revision_url")
+    private String resourceRevisionUrl;
+
+}
