6909e31aa14908ff21e9de24e0ce32e3abd39555
Gesserok <if2102@outlook.com>
2020-11-03 15:15:54 +0200

    exclude = HibernateJpaAutoConfiguration.class

--- application/src/main/java/org/example/multimodule/application/DemoApplication.java
+++ application/src/main/java/org/example/multimodule/application/DemoApplication.java
@@ -1,12 +1,13 @@
 package org.example.multimodule.application;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@SpringBootApplication(scanBasePackages = "org.example.multimodule")
+@SpringBootApplication(scanBasePackages = "org.example.multimodule", exclude = HibernateJpaAutoConfiguration.class)
 @RestController
 public class DemoApplication extends SpringBootServletInitializer {
 
