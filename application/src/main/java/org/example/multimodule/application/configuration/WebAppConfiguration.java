e8c410d6f8ea464bc475f331422bf2a7fe207e84
Gesserok <if2102@outlook.com>
2020-11-12 13:41:06 +0200

    +

--- application/src/main/java/org/example/multimodule/application/configuration/WebAppConfiguration.java
+++ application/src/main/java/org/example/multimodule/application/configuration/WebAppConfiguration.java
@@ -3,8 +3,14 @@ package org.example.multimodule.application.configuration;
 import lombok.extern.log4j.Log4j2;
 import org.example.multimodule.infrastructure.LoggerConfigurator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationPreparedEvent;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.boot.context.event.ApplicationStartingEvent;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.context.ApplicationEvent;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ApplicationContextEvent;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.ContextStartedEvent;
 import org.springframework.context.event.EventListener;
@@ -42,6 +48,48 @@ public class WebAppConfiguration {
 //        });
 //        return (ConfigurationStoredParameters) result;
 //    }
+
+    @EventListener(WebServerInitializedEvent.class)
+    public void webServerInitializedEvent() {
+        loggerConfigurator.reconfigure();
+        log.info("WebServerInitializedEvent. Logger reconfigured.");
+    }
+
+    @EventListener(ApplicationEvent.class)
+    public void appContext1() {
+        loggerConfigurator.reconfigure();
+        log.info("ApplicationEvent. Logger reconfigured.");
+    }
+
+    @EventListener(ApplicationPreparedEvent.class)
+    public void appPrep() {
+        loggerConfigurator.reconfigure();
+        log.info("ApplicationPreparedEvent. Logger reconfigured.");
+    }
+
+    @EventListener(ApplicationStartedEvent.class)
+    public void appStarted() {
+        loggerConfigurator.reconfigure();
+        log.info("ApplicationStartedEvent. Logger reconfigured.");
+    }
+
+    @EventListener(ApplicationStartingEvent.class)
+    public void appStarting() {
+        loggerConfigurator.reconfigure();
+        log.info("ApplicationStartingEvent. Logger reconfigured.");
+    }
+
+    @EventListener(ApplicationContextEvent.class)
+    public void appContext() {
+        loggerConfigurator.reconfigure();
+        log.info("ApplicationContextEvent. Logger reconfigured.");
+    }
+
+    @EventListener(ContextStartedEvent.class)
+    public void start() {
+        loggerConfigurator.reconfigure();
+        log.info("ContextRefreshedEvent. Logger reconfigured.");
+    }
 
     @EventListener(ContextRefreshedEvent.class)
     public void refresh() {
