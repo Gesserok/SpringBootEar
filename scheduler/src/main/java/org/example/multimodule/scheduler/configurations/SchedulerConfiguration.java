953825ac4afe17c34683f686696434a89b5e9e1e
Gesserok <if2102@outlook.com>
2020-11-14 14:11:50 +0200

    Scheduling not working

--- scheduler/src/main/java/org/example/multimodule/scheduler/configurations/SchedulerConfiguration.java
+++ scheduler/src/main/java/org/example/multimodule/scheduler/configurations/SchedulerConfiguration.java
@@ -1,9 +1,20 @@
 package org.example.multimodule.scheduler.configurations;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.time.LocalDateTime;
+
 @Configuration
 @EnableScheduling
+@Log4j2
 public class SchedulerConfiguration {
+
+    @Bean
+    public LocalDateTime localDateTime() {
+        log.info("-------------------LocalDateTime------------------------------");
+        return LocalDateTime.now();
+    }
 }
