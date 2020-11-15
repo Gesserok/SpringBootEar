54ca59a7818ba583a1241ee6c3ba689c232e3e53
Gesserok <if2102@outlook.com>
2020-11-15 17:32:56 +0200

    fix nullpointerexception

--- library/src/main/java/org/example/multimodule/annotations/constraints/ResponseResourceShowValidator.java
+++ library/src/main/java/org/example/multimodule/annotations/constraints/ResponseResourceShowValidator.java
@@ -1,18 +1,20 @@
-package org.example.multimodule.annotations.constraints;
-
-import org.example.multimodule.dto.ResponseResourceShow;
-
-import javax.validation.ConstraintValidator;
-import javax.validation.ConstraintValidatorContext;
-
-public class ResponseResourceShowValidator implements ConstraintValidator<ValidResponse, ResponseResourceShow> {
-    @Override
-    public void initialize(ValidResponse validResponse) {
-
-    }
-
-    @Override
-    public boolean isValid(ResponseResourceShow responseResourceShow, ConstraintValidatorContext constraintValidatorContext) {
-        return false;
-    }
-}
+//package org.example.multimodule.annotations.constraints;
+//
+//import org.example.multimodule.dto.ResponseResourceShow;
+//
+//import javax.validation.ConstraintValidator;
+//import javax.validation.ConstraintValidatorContext;
+//
+//public class ResponseResourceShowValidator implements ConstraintValidator<ValidResponse, ResponseResourceShow> {
+//    @Override
+//    public void initialize(ValidResponse validResponse) {
+//
+//    }
+//
+//    @Override
+//    public boolean isValid(ResponseResourceShow responseResourceShow, ConstraintValidatorContext constraintValidatorContext) {
+//
+//
+//        return false;
+//    }
+//}
--- library/src/main/java/org/example/multimodule/services/client/impl/ResourceShowRevisionResolverImpl.java
+++ library/src/main/java/org/example/multimodule/services/client/impl/ResourceShowRevisionResolverImpl.java
@@ -6,12 +6,16 @@ import org.example.multimodule.annotations.constraints.ValidResponse;
 import org.example.multimodule.dto.ResponseResourceShow;
 import org.example.multimodule.models.ResourceTask;
 import org.example.multimodule.models.package_show.Resource;
+import org.example.multimodule.models.resource_show.ResourceRevision;
 import org.example.multimodule.services.client.ODPClient;
 import org.example.multimodule.services.client.ResourceShowRevisionResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor(onConstructor = @__({@Autowired}))
@@ -23,15 +27,34 @@ public class ResourceShowRevisionResolverImpl implements ResourceShowRevisionRes
     @Override
     public List<ResourceTask> getAll(Resource resource) {
 
-        @ValidResponse
-        ResponseResourceShow resource_show = odpClient.getResponse(
+//        @ValidResponse
+        ResponseResourceShow resourceShow = odpClient.getResponse(
                 "resource_show",
                 resource.getId(),
                 ResponseResourceShow.class
         );
 
+        if (Objects.isNull(resourceShow.getResult())) {
+            log.warn("Field \"Result\" is null");
+            return new ArrayList<>();
+        }
+
+        if (Objects.isNull(resourceShow.getResult().getResourceRevisions())) {
+            log.warn("Field \"Result.ResourceRevisions\" is null");
+            List<ResourceTask> resourceTasks = new ArrayList<>();
+            resourceTasks.add(new ResourceTask(resourceShow.getResult()));
+            return resourceTasks;
+        }
+
+        List<ResourceRevision> resourceRevisions = resourceShow.getResult().getResourceRevisions();
+
+        return Objects.requireNonNull(resourceRevisions)
+                .stream()
+                .map(resourceRevision -> new ResourceTask(resource, resourceRevision))
+                .peek(resourceTask -> log.debug("Resource added to list"))
+                .collect(Collectors.toList());
 
 
-        return null;
+        return ;
     }
 }
