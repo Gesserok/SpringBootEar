1cf703ececeb25ec2acbf22febe6fa647ecfac71
Gesserok <if2102@outlook.com>
2020-11-15 17:34:33 +0200

    fix compiling

--- library/src/main/java/org/example/multimodule/services/client/impl/ResourceShowRevisionResolverImpl.java
+++ library/src/main/java/org/example/multimodule/services/client/impl/ResourceShowRevisionResolverImpl.java
@@ -2,7 +2,6 @@ package org.example.multimodule.services.client.impl;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.example.multimodule.annotations.constraints.ValidResponse;
 import org.example.multimodule.dto.ResponseResourceShow;
 import org.example.multimodule.models.ResourceTask;
 import org.example.multimodule.models.package_show.Resource;
