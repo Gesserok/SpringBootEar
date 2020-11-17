c3708cfc0d43fd43208ac0f43b9cc96e6069b115
Gesserok <if2102@outlook.com>
2020-11-17 15:25:36 +0200

    res

--- scheduler/src/main/java/org/example/multimodule/scheduler/tasks/ResourceTaskExecutor.java
+++ scheduler/src/main/java/org/example/multimodule/scheduler/tasks/ResourceTaskExecutor.java
@@ -9,15 +9,15 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 
-//@Service
-//@Scope(
-//        value = ConfigurableBeanFactory.SCOPE_PROTOTYPE,
-//        proxyMode = ScopedProxyMode.TARGET_CLASS)
-//@AllArgsConstructor(onConstructor = @__({@Autowired}))
+@Service
+@Scope(
+        value = ConfigurableBeanFactory.SCOPE_PROTOTYPE,
+        proxyMode = ScopedProxyMode.TARGET_CLASS)
+@AllArgsConstructor(onConstructor = @__({@Autowired}))
 public class ResourceTaskExecutor implements Runnable{
 
-    private ResourceTask resourceTask;
-    private ResourceTaskLoader resourceTaskLoader;
+    private final ResourceTask resourceTask;
+    private final ResourceTaskLoader resourceTaskLoader;
 
     @Override
     public void run() {
