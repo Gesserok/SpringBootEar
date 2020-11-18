cf5a539745c17901007a29a0976a334b3016ec32
Gesserok <if2102@outlook.com>
2020-11-18 16:59:55 +0200

    rnk

--- library/src/main/java/org/example/multimodule/dao/ResourceTaskRepository.java
+++ library/src/main/java/org/example/multimodule/dao/ResourceTaskRepository.java
@@ -14,17 +14,17 @@ public interface ResourceTaskRepository extends JpaRepository<ResourceTask, Long
     ResourceTask findFirstByNameIgnoreCaseOrderByDateRevisionDescDateRevisionDesc(String name);
 
     @Query(value = "SELECT rt.id, rt.name, rt.added_time, rt.day_revision, rt,date_revision, rt.url, rt.upload_time " +
-            "FROM (SELECT id," +
-            "             name," +
-            "             added_time," +
-            "             day_revision," +
-            "             date_revision," +
-            "             url," +
-            "             upload_time," +
-            "             rank()" +
-            "             over (partition by name order by day_revision, date_revision desc) rnk" +
-            "      from odp.resource_tasks) rt" +
-            "WHERE rnk = 1", nativeQuery = true)
+            "FROM (SELECT id, " +
+            "             name, " +
+            "             added_time, " +
+            "             day_revision, " +
+            "             date_revision, " +
+            "             url, " +
+            "             upload_time, " +
+            "             rank() " +
+            "             over (partition by name order by day_revision, date_revision desc) rnk " +
+            "      from odp.resource_tasks) rt " +
+            "WHERE rnk = 1 ", nativeQuery = true)
     List<ResourceTask> findAllGroupByNameAndUploadTimeIsNullOrderByDateRevisionDescDateRevisionDesc();
 
 }
