81353fd62ec2a4dc37a444aa9e3b08ad333969d7
Gesserok <if2102@outlook.com>
2020-11-23 14:35:56 +0200

    qweqwe

--- execution_locker/src/main/java/org/example/task_locker/aspects/LockedAspectProcessor.java
+++ execution_locker/src/main/java/org/example/task_locker/aspects/LockedAspectProcessor.java
@@ -1,37 +1,32 @@
 package org.example.task_locker.aspects;
 
-import lombok.AllArgsConstructor;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.example.task_locker.annotations.ExecutionLocking;
-import org.example.task_locker.services.TaskLockService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
 
 @Aspect
 @Component
-@AllArgsConstructor(onConstructor = @__({@Autowired}))
 public class LockedAspectProcessor {
-    private final TaskLockService taskLockService;
 
-    @Pointcut(value = "@annotation(executionLocking)", argNames = "executionLocking")
-    private void annotatedMethods(ExecutionLocking executionLocking) {
-
-    }
-
-    @Pointcut(value = "execution(public * * (..)) && @within(executionLocking)", argNames = "executionLocking")
-    private void methodOfAnnotatedClass(ExecutionLocking executionLocking) {
+    @Pointcut(value = "@annotation(executionLocking)", argNames = "joinPoint,executionLocking")
+    private void annotatedMethods(JoinPoint joinPoint, ExecutionLocking executionLocking) {
 
     }
 
-    @Pointcut(value = "annotatedMethods(executionLocking) || methodOfAnnotatedClass(executionLocking)", argNames = "executionLocking")
-    private void annotatedMethodOrClass(ExecutionLocking executionLocking) {
+    @Pointcut(value = "execution(public * * (..)) && @within(executionLocking)", argNames = "joinPoint,executionLocking")
+    private void methodOfAnnotatedClass(JoinPoint joinPoint, ExecutionLocking executionLocking) {
 
     }
 
-    @Before(value = "annotatedMethodOrClass(executionLocking)", argNames = "joinPoint,executionLocking")
+    @Pointcut(value = "annotatedMethods(joinPoint,executionLocking) || methodOfAnnotatedClass(joinPoint,executionLocking)", argNames = "joinPoint,executionLocking")
+    private void annotatedMethodOrClass(JoinPoint joinPoint, ExecutionLocking executionLocking) {
+
+    }
+
+    @Before(value = "annotatedMethodOrClass(joinPoint,executionLocking)", argNames = "joinPoint,executionLocking")
     public void beforeAdvice(JoinPoint joinPoint, ExecutionLocking executionLocking) {
         Method[] declaredMethods = joinPoint.getTarget().getClass().getDeclaredMethods();
 
@@ -40,13 +35,13 @@ public class LockedAspectProcessor {
 
     }
 
-    @AfterReturning(value = "annotatedMethodOrClass(executionLocking)", argNames = "executionLocking")
-    public void afterReturningAdvice(ExecutionLocking executionLocking) {
+    @AfterReturning(value = "annotatedMethodOrClass(joinPoint,executionLocking)", argNames = "joinPoint,executionLocking")
+    public void afterReturningAdvice(JoinPoint joinPoint, ExecutionLocking executionLocking) {
 
     }
 
-    @AfterThrowing(value = "annotatedMethodOrClass(executionLocking)", argNames = "executionLocking")
-    public void afterThrowingAdvice(ExecutionLocking executionLocking) {
+    @AfterThrowing(value = "annotatedMethodOrClass(joinPoint,executionLocking)", argNames = "joinPoint,executionLocking")
+    public void afterThrowingAdvice(JoinPoint joinPoint, ExecutionLocking executionLocking) {
 
     }
 
