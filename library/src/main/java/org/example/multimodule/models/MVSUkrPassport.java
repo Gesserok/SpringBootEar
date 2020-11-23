5d5ff75e89aaa7ec744219c9b682dd1ce30f36e0
Gesserok <if2102@outlook.com>
2020-11-23 09:19:04 +0200

    +

--- library/src/main/java/org/example/multimodule/models/MVSUkrPassport.java
+++ library/src/main/java/org/example/multimodule/models/MVSUkrPassport.java
@@ -4,13 +4,18 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.persistence.Table;
 
 @NoArgsConstructor
+@Data
+@AllArgsConstructor
 @Embeddable
 @Table(name = "mvs_ukr_passports")
 @JsonIgnoreProperties(ignoreUnknown = true)
