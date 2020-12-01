87177929389e6fb6e18580d6de88d8ec4889ff1d
Gesserok <if2102@outlook.com>
2020-12-01 14:31:41 +0200

    version

---
+++ version/src/main/java/org/example/multimodule/version/DemoApplication.java
@@ -0,0 +1,18 @@
+package org.example.multimodule.version;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication(scanBasePackages = "org.example.multimodule.version", exclude = HibernateJpaAutoConfiguration.class)
+@RestController
+public class DemoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
+    }
+}
