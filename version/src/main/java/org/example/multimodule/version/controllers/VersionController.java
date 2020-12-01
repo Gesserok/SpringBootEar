d4c8f0d806b56bab96071787b9dc8b8c540c94a6
Gesserok <if2102@outlook.com>
2020-12-01 14:37:19 +0200

    version +

--- version/src/main/java/org/example/multimodule/version/controllers/VersionController.java
+++ version/src/main/java/org/example/multimodule/version/controllers/VersionController.java
@@ -1,6 +1,7 @@
 package org.example.multimodule.version.controllers;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
