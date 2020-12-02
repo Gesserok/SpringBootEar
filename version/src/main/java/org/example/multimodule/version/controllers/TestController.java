4ffe75152c744376c7e443b0fa9afe6741ca08b9
Gesserok <if2102@outlook.com>
2020-12-02 16:41:17 +0200

    +

---
+++ version/src/main/java/org/example/multimodule/version/controllers/TestController.java
@@ -0,0 +1,22 @@
+package org.example.multimodule.version.controllers;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletContext;
+
+@RestController
+@Log4j2
+public class TestController {
+
+    @Autowired
+    private ServletContext servletContext;
+
+    @GetMapping("/servlet")
+    public String version() {
+        return "version";
+    }
+
+}
